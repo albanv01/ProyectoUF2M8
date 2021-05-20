c48ad398c5a14cbe81c5b5c397383693d80c3856
Alba Navarro Vargas <vargasnavarroalba@gmail.com>
2021-05-20 11:01:24 +0200

    put markers and changed style

--- app/src/main/java/com/example/incivisme/MapaFragment.java
+++ app/src/main/java/com/example/incivisme/MapaFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -28,7 +29,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -45,13 +45,20 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
 
     // below are the latitude and longitude
     // of 4 different locations.
-    LatLng sydney = new LatLng(-34, 151);
-    LatLng TamWorth = new LatLng(-31.083332, 150.916672);
-    LatLng NewCastle = new LatLng(-32.916668, 151.750000);
-    LatLng Brisbane = new LatLng(-27.470125, 153.021072);
+    LatLng punto1 = new LatLng(41.389996599694406, 2.1352847162868778);
+    LatLng punto2 = new LatLng(41.3978520048405, 2.1483309816120335);
+    LatLng punto3 = new LatLng(41.362752884556315, 2.1395762509333105);
+    LatLng punto4 = new LatLng(41.38479472864416, 2.1645560329064213);
+    LatLng punto5 = new LatLng(41.38119745632779, 2.13339243922842);
+    LatLng punto6 = new LatLng(41.37580117473925, 2.047093256735496);
+    LatLng punto7 = new LatLng(41.36980478119577, 2.0570815880425473);
+    LatLng punto8 = new LatLng(41.36800575534873, 2.0774577839089323);
+    LatLng punto9 = new LatLng(41.43079228248801, 2.1593621006267543);
+    LatLng punto10 = new LatLng(41.73142475940853, 1.8268225202151263);
+    LatLng punto11 = new LatLng(41.41704013932239, 2.0161956088627764);
 
     private ArrayList<LatLng> locationArrayList;
-
+    private Object BitmapDescriptor;
 
 
     @Override
@@ -69,17 +76,24 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
 
         // on below line we are adding our
         // locations in our array list.
-        locationArrayList.add(sydney);
-        locationArrayList.add(TamWorth);
-        locationArrayList.add(NewCastle);
-        locationArrayList.add(Brisbane);
+        locationArrayList.add(punto1);
+        locationArrayList.add(punto2);
+        locationArrayList.add(punto3);
+        locationArrayList.add(punto4);
+        locationArrayList.add(punto5);
+        locationArrayList.add(punto6);
+        locationArrayList.add(punto7);
+        locationArrayList.add(punto8);
+        locationArrayList.add(punto9);
+        locationArrayList.add(punto10);
+        locationArrayList.add(punto11);
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         DatabaseReference base = FirebaseDatabase.getInstance("https://incivisme-c4af7-default-rtdb.firebaseio.com/").getReference();
 
         DatabaseReference users = base.child("users");
         DatabaseReference uid = users.child(auth.getUid());
-        DatabaseReference incidencies = uid.child("incidencies");
+        DatabaseReference incidencies = uid.child("opinions");
 
         SharedViewModel model = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
 
@@ -149,14 +163,10 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-        // inside on map ready method
-        // we will be displaying all our markers.
-        // for adding markers we are running for loop and
-        // inside that we are drawing marker on our map.
         for (int i = 0; i < locationArrayList.size(); i++) {
 
             // below line is use to add marker to each location of our array list.
-            mMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
+            mMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Punt de Recollida").icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_delete_outline_24)));
 
             // below lin is use to zoom our camera on map.
             mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
@@ -164,6 +174,14 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
             // below line is use to move our camera to the specific location.
             mMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
         }
+    }
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
     }
 
